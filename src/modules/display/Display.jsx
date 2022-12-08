21e81b2a4a03e4df0847b61fcec90f0938d95d0d
Joe <caspar0912@googlemail.com>
2022-12-08 17:28:19 +0000

    correct bug with display loading when no details present

--- src/modules/display/Display.jsx
+++ src/modules/display/Display.jsx
@@ -10,17 +10,21 @@ import InterestsDisplay from './InterestsDisplay';
 const Display = (props) => {
   const { details, pdfRef } = props;
   return (
-    <section className="sticky top-4 w-full lg:w-3/5 h-full shadow-black shadow-md dark:shadow-slate-500">
-      <div ref={pdfRef} className=" w-full h-full bg-zinc-200">
-        <Header details={details} />
-        <SkillsDisplay details={details} />
-        <ProjectsDisplay details={details} />
-        <ProfessionalDisplay details={details} />
-        <CoursesDisplay details={details} />
-        <EducationDisplay details={details} />
-        <InterestsDisplay details={details} />
-      </div>
-    </section>
+    <>
+      {(details.personal.name || details.personal.role) && (
+        <section className="sticky top-4 w-full lg:w-3/5 h-full shadow-black shadow-md dark:shadow-slate-500">
+          <div ref={pdfRef} className=" w-full h-full bg-zinc-200">
+            <Header details={details} />
+            <SkillsDisplay details={details} />
+            <ProjectsDisplay details={details} />
+            <ProfessionalDisplay details={details} />
+            <CoursesDisplay details={details} />
+            <EducationDisplay details={details} />
+            <InterestsDisplay details={details} />
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
